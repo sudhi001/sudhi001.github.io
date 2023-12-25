ebc54687b0f6bed023dfe1ce30443c011e421584
Sudhi S <sudhis@live.com>
2023-12-25 19:07:09 +0530

    feat:inital

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
-  assetPrefix: './',
 }
 module.exports = nextConfig
