4a653d592528dd025b7d8c5f9f58b60aa38f7447
Sudhi S <sudhis@live.com>
2023-12-25 23:27:11 +0530

    feat:ui fixes

--- components/profile/profile_image.js
+++ components/profile/profile_image.js
@@ -2,8 +2,8 @@ import Image from 'next/image';
 
 export default function ProfileImage(props) {
     return <div className="flex items-center justify-center mt-7">
-        <Image src={'/profile.JPG'}
-            alt=" Profile Image"
+        <Image src="/profile.jpg"
+            alt="Profile Image"
             width={200}
             height={200}
             className="inline-block h-40 w-40 rounded-full ring-2 dark:ring-white  ring-black object-cover "
