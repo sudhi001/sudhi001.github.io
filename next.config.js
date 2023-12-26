f49c555265f3ccaa8e30ba6c65aecb6b736a0a46
Sudhi S <sudhis@live.com>
2023-12-26 07:18:21 +0530

    feat: image bug fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isDev = process.env.NODE_ENV === 'development';
 const nextConfig = {
-   ...(!isDev ? { output: 'export' } : {}),
+    output: 'export' 
 }
 module.exports = nextConfig
 
