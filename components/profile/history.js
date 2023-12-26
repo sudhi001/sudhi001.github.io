ab447e9f57cd49541669f308116ce74d82c894af
Sudhi S <sudhis@live.com>
2023-12-26 10:32:41 +0530

    feat: image bug fix

--- components/profile/history.js
+++ components/profile/history.js
@@ -2,10 +2,10 @@ import data from '../../app/data';
 
 export default function EmploymentHistory(props) {
     return <div className="max-w-full mx-auto  lg:pl-24 lg:pr-24  lg:pb-7  p-4">
-        <h2 className="text-2xl font-bold mb-4 underline font-mono">{data.employmentHistory.title}</h2>
+        <h1 className="text-2xl font-bold mb-4 underline font-mono">{data.employmentHistory.title}</h1>
         {data.employmentHistory.data.map((item, index) => (
             <div className="mb-6" key={index}>
-                <p className="text-xl font-semibold font-mono ">{item.position}, {item.company}</p>
+                <h1 className="text-xl font-semibold font-mono ">{item.position}, {item.company}</h1>
                 <p className="pb-4 font-mono">{item.duration}</p>
                 <ul className="list-disc pl-6 font-mono">
                     {item.responsibilities.map((item, kindex) => (
--- components/profile/projects.js
+++ components/profile/projects.js
@@ -2,10 +2,10 @@ import data from '../../app/data';
 
 export default function Projects(props) {
     return <div className="max-w-full mx-auto  lg:pl-24 lg:pr-24  lg:pb-7  p-4">
-        <h2 className="text-2xl font-bold mb-4 underline font-mono">{data.projects.title}</h2>
+        <h1 className="text-2xl font-bold mb-4 underline font-mono">{data.projects.title}</h1>
         {data.projects.data.map((item, index) => (
             <div className="mb-6" key={index}>
-                <p className="text-xl font-semibold font-mono ">{item.title}</p>
+                <h1 className="text-xl font-semibold font-mono ">{item.title}</h1>
                 <a key={index} href={item.href} className="font-mono" target="_blank" rel="" >{item.href}</a>
                 <ul className="list-disc pl-6 font-mono mt-4">
                     {item.descriptions.map((item, kindex) => (
