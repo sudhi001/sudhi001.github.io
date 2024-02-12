fe95b2e1db5e6331a8c6a67f29452a50abea49f6
Sudhi S <sudhis@live.com>
2024-02-12 08:55:56 +0530

    new

--- app/lite.tsx
+++ app/lite.tsx
@@ -1,19 +1,23 @@
-'use client'
-import {NextUIProvider} from "@nextui-org/react";
-import ProfilePrimaryBio from '../components/profile/primary_bio'
-import ProfileAboutMe from '../components/profile/about_me'
-import ProfileImage from '../components/profile/profile_image'
-import SocialLink from '../components/profile/social_link'
-import EmploymentHistory from '../components/profile/history'
-import Projects from '../components/profile/projects'
+import { useState, useEffect } from "react";
+import { NextUIProvider } from "@nextui-org/react";
+import ProfilePrimaryBio from '../components/profile/primary_bio';
+import ProfileAboutMe from '../components/profile/about_me';
+import ProfileImage from '../components/profile/profile_image';
+import SocialLink from '../components/profile/social_link';
+import EmploymentHistory from '../components/profile/history';
+import Projects from '../components/profile/projects';
+
 
 export default function Home() {
   const currentDate = new Date();
   const currentDateTimeString = currentDate.toLocaleString('en-US');
   return (
     <NextUIProvider>
+    
     <main className="flex min-h-screen flex-col items-center lg:items-start ">
+      
       <div className="flex grid  lg:grid-cols-2  items-center lg:items-start">
+        
         <ProfileImage />
         <div className="pt-7">
           <ProfilePrimaryBio />
@@ -24,7 +28,7 @@ export default function Home() {
       <EmploymentHistory />
       <Projects/>
      <div className='flex max-w-full mx-auto mt-14  items-center justify-center'>
-      <p className='font-mono text-sm'>Updated On {currentDateTimeString}</p>
+      <p className='font-mono text-sm'>Updated On</p>
      </div>
     </main>
     </NextUIProvider>
