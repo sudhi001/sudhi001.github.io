5b0c794268014740f2b284726708a33cb99f7c7f
Sudhi S <sudhi001@users.noreply.github.com>
2025-01-20 19:23:50 +0530

    Update data.js

--- app/data.js
+++ app/data.js
@@ -127,7 +127,14 @@ const data = {
   projects:{
     title:"Open Source Projects",
     data:[
-      
+       {
+        title:"Android MacroBenchmarkViewer",
+        descriptions:[
+          "Android MacroBenchmark Viewer is an open-source tool designed to visualize Android microbenchmark data directly in your browser.",
+          " It allows you to upload a JSON file containing benchmark results, view device and hardware specifications, and render charts (line and bar) for key performance metrics."
+        ],
+        href:"https://github.com/sudhi001/AndroidMacroBenchmarkViewer"
+      },
       {
         title:"crypto_utils",
         descriptions:[
